085a7eddf2deb446b8794a7f47dee850a68a4356
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-05-01 05:43:59 +0700

    add new bfs function which returns ArrayList

--- src/main/java/ModNav/ModNavStructure/Algorithm/Traverse.java
+++ src/main/java/ModNav/ModNavStructure/Algorithm/Traverse.java
@@ -1,9 +1,6 @@
 package ModNav.ModNavStructure.Algorithm;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 import ModNav.ModNavStructure.ModNavGraph;
 import ModNav.ModNavStructure.Path;
@@ -34,6 +31,29 @@ public class Traverse {
         }
 
         return str.toString();
+    }
 
+    public ArrayList<Place> bfsList(ModNavGraph graph, Place startNode){
+        Map<Place, Boolean> visited = new HashMap<>();
+        ArrayList<Place> l = new ArrayList<>();
+        Queue<Place> q = new LinkedList<>(); //queue สำเร็จรูป
+
+        q.add(startNode);
+        visited.put(startNode, true);
+
+        while(!q.isEmpty()){
+            Place current = q.peek(); //access สมาชิกตัวหน้า q
+            q.remove(); // pop q
+            l.add(current);
+
+            for(Path i : graph.getAdjacencyList(current)){
+                if(!visited.containsKey(i.getDest()) || !visited.get(i.getDest())){
+                    q.add(i.getDest());
+                    visited.put(i.getDest(), true);
+                }
+            }
+        }
+
+        return l;
     }
 }
