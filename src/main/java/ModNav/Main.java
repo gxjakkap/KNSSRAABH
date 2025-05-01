14a9e91cc6861bbff1f796d6566facf783d95deb
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-05-02 01:41:06 +0700

    add main loop (WIP)

--- src/main/java/ModNav/Main.java
+++ src/main/java/ModNav/Main.java
@@ -18,30 +18,35 @@ public class Main {
         // Main loop
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to KMUTTNavigationSuperSmartRetroAncientAutomaticButHandtomatic");
-        System.out.println("(Or KNSSRAABH for short.)");
+        do {
+            System.out.println("Welcome to KMUTTNavigationSuperSmartRetroAncientAutomaticButHandtomatic");
+            System.out.println("(Or KNSSRAABH for short.)");
 
-        System.out.println("================== Main Menu ==================");
-        System.out.print("[1] Search with BID\t\t\t[2] Search with name\n");
-        System.out.print("[3] Add new place\t\t\t[4] Add new path\n");
+            System.out.println("================== Main Menu ==================");
+            System.out.print("[1] Search with BID\t\t\t[2] Search with name\n");
+            System.out.print("[3] Add new place\t\t\t[4] Add new path\n");
+            System.out.print("[5] Exit\n");
 
-        int opts = UserInputs.getIntegerInput(sc, 1, 4,"> Option: ");
+            int opts = UserInputs.getIntegerInput(sc, 1, 4, "> Option: ");
 
-        switch (opts){
-            case 1:
+            if (opts == 1) {
                 MainMenuFunctions.searchByBID(sc, g);
-                break;
-            case 2:
-                break;
-            case 3:
+            }
+//            else if (opts == 2) {
+//              TODO: Search with name
+//            }
+            else if (opts == 3) {
                 MainMenuFunctions.addNewPlace(sc, g);
-                break;
-            case 4:
+            }
+            else if (opts == 4) {
                 MainMenuFunctions.addPath(sc, g);
+            }
+            else if (opts == 5) {
                 break;
-            default:
-                break;
+            }
         }
+        while (true);
 
+        sc.close();
     }
 }
